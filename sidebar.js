7b2414586485b6fc8546f0427c31e56bc2ca5724
JaneJane21 <101927310+JaneJane21@users.noreply.github.com>
2022-11-20 13:21:35 +0000

    2

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,6 @@
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("burger").style.display = "none";
-    document.getElementById("burger").style.display = "none";
   }
   
   function w3_close() {
